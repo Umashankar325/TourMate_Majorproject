f0f81ac9f06089cd6563c16c147bbb0a65c78336
Umashnkar Uikey <umashankaruikey325@gmail.com>
2025-05-04 23:39:58 +0530

    globaldb

--- Backend/src/db/db.js
+++ Backend/src/db/db.js
@@ -1,10 +1,13 @@
-const mongoose=require('mongoose');
-const connect=async()=>{
-    try {
-        await mongoose.connect("mongodb://localhost:27017/nodeturism", );
-        console.log("MongoDB connected successfully");
-    } catch (error) {
-        console.error("MongoDB connection error:", error);
-    }
-}
-module.exports=connect;
+const mongoose = require("mongoose");
+const connect = async () => {
+  try {
+    // await mongoose.connect("mongodb://localhost:27017/nodeturism", );
+    await mongoose.connect(
+      "mongodb+srv://umashankaruikey325:k3NbonZ2E8Ktvluy@insta-post-project.qam8k.mongodb.net/"
+    );
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+  }
+};
+module.exports = connect;
